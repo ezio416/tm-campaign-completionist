3b0014414e9cea8f9db0c58863d5230bd7065684
Ezio <e@e416.dev>
2025-03-10 23:16:01 -0600

    type hints

--- build.py
+++ build.py
@@ -1,43 +1,43 @@
 # c 2023-12-28
-# m 2024-07-21
+# m 2025-03-10
 
 import os
 from zipfile import ZipFile, ZIP_DEFLATED
 
 
 def main():
-    dir: str = os.getcwd()
+    dir = os.getcwd()
 
-    src: str = dir + '/src'
+    src = dir + '/src'
     if not os.path.isdir(src):
         print('src folder missing!')
         return
 
-    has_assets: bool = True
-    assets: str = dir + '/assets'
+    has_assets = True
+    assets = dir + '/assets'
     if not os.path.isdir(assets):
         print('assets folder missing!')
         has_assets = False
 
-    license: str = dir + '/LICENSE.txt'
+    license = dir + '/LICENSE.txt'
     if not os.path.isfile(license):
         print('LICENSE.txt missing!')
         return
 
-    info: str = dir + '/info.toml'
+    info = dir + '/info.toml'
     if not os.path.isfile(info):
         print('info.toml missing!')
         return
 
     with open(info, 'r') as f:
-        lines: list[str] = f.readlines()
+        lines = f.readlines()
 
     for line in lines:
         if 'version' in line:
-            zip_name: str = dir.split('\\')[-1] + '_' + line.split(' ')[2].replace('"', '').replace('\n', '') + '.op'
+            zip_name = dir.split('\\')[-1] + '_' + line.split(' ')[2].replace('"', '').replace('\n', '') + '.op'
             break
 
-    new_zip_name: str = dir + '/versions/unsigned/' + zip_name
+    new_zip_name = dir + '/versions/unsigned/' + zip_name
 
     if os.path.isfile(new_zip_name):
         print(zip_name + ' already exists in unsigned folder!')
@@ -49,13 +49,13 @@ def main():
 
         for dir, subdirs, files in os.walk(src):
             for file in files:
-                abspath: str = os.path.join(dir, file)
+                abspath = os.path.join(dir, file)
                 z.write(abspath, os.path.relpath(abspath, os.path.join(src, '..')))
 
         if has_assets:
             for dir, subdirs, files in os.walk(assets):
                 for file in files:
-                    abspath: str = os.path.join(dir, file)
+                    abspath = os.path.join(dir, file)
                     z.write(abspath, os.path.relpath(abspath, os.path.join(assets, '..')))
 
     os.rename(zip_name, new_zip_name)
